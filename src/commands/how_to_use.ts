c79c01722d6ac2f0785fea6cabd3f9eeabaecb3a
denivladislav <deni.vladislav@gmail.com>
2025-10-04 17:16:32 +0300

    fix: improve how_to_use text

--- src/commands/how_to_use.ts
+++ src/commands/how_to_use.ts
@@ -12,10 +12,6 @@ const POLL_ANONYMOUS = (process.env.POLL_ANONYMOUS ?? 'false').toLowerCase() ===
 const POLL_ALLOW_MULTIPLE = (process.env.POLL_ALLOW_MULTIPLE ?? 'true').toLowerCase() === 'true';
 const TELEGRAM_MAX_OPTIONS = 10;
 
-// function escapeMarkdownV2(text: string): string {
-//   return text.replace(/([_*\[\]()~`>#+\-=|{}.!])/g, '\\$1');
-// }
-
 const how_to_use = () => async (ctx: Context) => {
   try {
     debug('Triggered /help command');
@@ -34,19 +30,18 @@ const how_to_use = () => async (ctx: Context) => {
 Использование:
 /start_poll Вопрос | Вариант 1 | Вариант 2 | ...
 
-Примеры:
-Создать опрос с дефолтными вариантами:
+Создать опрос с вариантами по умолчанию:
 /start_poll Го в баскет? | В пн | Во вт | В среду
-Варианты: В пн, Во вт, В среду, ${defaultOptsDisplay}
+Итог: В пн, Во вт, В среду, ${defaultOptsDisplay}
 
-Создать опрос без дефолтных вариантов:
+Создать опрос без вариантов по умолчанию:
 /start_poll Го в баскет? | В пн | Во вт --no-defaults
-Варианты: только В пн, Во вт
+Итог: только В пн, Во вт
 
-Правила:
+Текущие настройки:
 - Используйте символ | для разделения вариантов.
 - Telegram позволяет до ${TELEGRAM_MAX_OPTIONS} вариантов.
-- Дефолтные варианты: ${defaultOptsDisplay}
+- Варианты по умолчанию: ${defaultOptsDisplay}
 - Анонимный опрос: ${POLL_ANONYMOUS ? 'Да' : 'Нет'}
 - Разрешено несколько ответов: ${POLL_ALLOW_MULTIPLE ? 'Да' : 'Нет'}
 
